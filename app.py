eee1f30afa05bc496272d979dce717bbef5cf8a0
emiliotl <talamante.emilio@gmail.com>
2020-07-17 01:50:04 -0700

    Added database and gdrive functionality for fetching data

--- app.py
+++ app.py
@@ -1,7 +1,18 @@
-from flask import Flask
+from flask import Flask, request
 from pandas import read_csv
+from oauth2client.service_account import ServiceAccountCredentials
+import gspread
+
+import dao
 
 app = Flask(__name__)
+data_frame = read_csv('kaggle_data/AirPassengers.csv')
+creds = ServiceAccountCredentials.from_json_keyfile_name(
+    '',
+    ['https://spreadsheets.google.com/feeds',
+     'https://www.googleapis.com/auth/drive']
+)
+sheet = gspread.authorize(creds).open("Sample").sheet1
 
 
 @app.route('/')
@@ -11,8 +22,34 @@ def home_page():
 
 @app.route('/get_pandas_data', methods=['GET'])
 def get_pandas_data():
-    air_passengers_data_frame = read_csv('kaggle_data/AirPassengers.csv')
-    return {air_passengers_data_frame['time'][0]: str(air_passengers_data_frame['value'][0])}
+    return {
+        index: value for (index, value) in
+        data_frame[request.args['column']][:int(request.args['rows'])].items()
+    }
+
+
+@app.route('/get_database_data', methods=['GET'])
+def get_database_data():
+    return {
+        index: value for (index, value) in
+        enumerate(dao.query_to_select_data(
+            f'select {request.args["column"]} '
+            f'from actor limit {request.args["rows"]}'))
+    }
+
+
+@app.route('/get_drive_data', methods=['GET'])
+def get_drive_data():
+    if request.args:
+        return {
+            index: value for (index, value) in
+            enumerate(sheet.col_values(request.args["column_number"]))
+        }
+    else:
+        return {
+            index: value for (index, value) in
+            enumerate(sheet.get_all_records())
+        }
 
 
 app.run(port=4995)
---
+++ dao.py
@@ -0,0 +1,36 @@
+import psycopg2
+
+
+class CursorFromConnection:
+    def __init__(self):
+        self.connection = None
+        self.cursor = None
+
+    def __enter__(self):
+        self.connection = psycopg2.connect(
+            host="localhost",
+            database="",
+            user="",
+            password=""
+        )
+        self.cursor = self.connection.cursor()
+        return self.cursor
+
+    def __exit__(self, exc_type, exc_val, exc_tb):
+        if exc_val is not None:
+            self.connection.rollback()
+        else:
+            self.cursor.close()
+            self.connection.commit()
+        self.connection.close()
+
+
+def query_to_insert_data(query):
+    with CursorFromConnection() as cursor:
+        cursor.execute(query)
+
+
+def query_to_select_data(query):
+    with CursorFromConnection() as cursor:
+        cursor.execute(query)
+        return [row[0] for row in cursor.fetchall()]
